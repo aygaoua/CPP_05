38528d9cafb6238eb36e463f95447ff9e033029c
aygaoua <zgaoua72@gmail.com>
2025-02-07 02:35:31 +0100

    make all the needed implementations is good !!

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,61 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat (): name("default name") {
+
+}
+
+Bureaucrat::Bureaucrat (Bureaucrat const & src): name(src.name), grade(src.grade) {
+
+}
+
+Bureaucrat::~Bureaucrat() {
+
+}
+
+Bureaucrat&		Bureaucrat::operator=(const Bureaucrat& b) {
+
+    this->grade = b.grade;
+}
+
+std::string     Bureaucrat::getName() const{
+
+    return (this->name);
+}
+
+int Bureaucrat::getGrade() const{
+
+    return (this->grade);
+}
+
+void            Bureaucrat::increment(int grades){
+
+    if (getGrade()  - grades < 1)
+    {
+        throw(GradeTooHighException());
+    }
+    grade -= grades;
+}
+
+void            Bureaucrat::decrement(int grades){
+
+    if (getGrade() + grades > 150)
+    {
+        throw(GradeTooLowException());
+    }
+    grade += grades;
+}
+
+std::ostream&	        operator<<(std::ostream& os, const Bureaucrat& obj){
+
+    os << obj.getName() << ", bureaucrat grade " << obj.getGrade();
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw(){
+
+	return "The Grade is Too High!";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw(){
+
+	return "The Grade is Too Low!";
+}
