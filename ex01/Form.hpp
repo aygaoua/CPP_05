c0205bfddb2187b8b0bc7701ea2449e13476978d
aygaoua <zgaoua72@gmail.com>
2025-02-08 20:59:21 +0100

    make the .hpp Form file great

--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -4,6 +4,8 @@
 #include <exception>
 #include "Bureaucrat.hpp"
 
+class Bureaucrat;
+
 class Form {
     private:
         std::string const   name;
@@ -12,14 +14,22 @@ class Form {
         int const           required_to_execute;
     public:
     /*----------------------- Orthodox Canonical Form -----------------------*/
-		Form		();
-		Form		(Form const & src);
-		Form&		operator =(const Form& b);
+		Form		        ();
+		Form		        (Form const & src);
+		Form&		        operator =(const Form& b);
 		~Form();
 	/*-----------------------------------------------------------------------*/
 
+    /*----------------------- public member functions -----------------------*/
+		Form		        (std::string name__, int grade_to_sign, int grade_to_ex);
+        std::string         getName() const;
+        bool                get_is_signed() const;
+        int                 get_required_to_sign() const;
+        int                 get_required_to_execute() const;
+        void                beSigned(const Bureaucrat &bureaucrat);
+	/*-----------------------------------------------------------------------*/
+
     /*----------------------- exceptions for the ex01 -----------------------*/
-		Form		(std::string name__, int grade__);
         class GradeTooHighException : public std::exception{
             const char* what() const throw();
         };
